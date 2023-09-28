4a60fa3c0f4edb1f065e99706e63145fecdc9769
유영 <dbdud1999@naver.com>
2023-09-29 00:22:38 +0900

    :art: 에러 메시지 수정

--- spring-workspace/Pair01_ParkSeungSoo_YouYoung/src/main/java/com/ssafy/trip/util/exception/common/CityInvalidException.java
+++ spring-workspace/Pair01_ParkSeungSoo_YouYoung/src/main/java/com/ssafy/trip/util/exception/common/CityInvalidException.java
@@ -7,7 +7,7 @@ public class CityInvalidException extends MyException {
 
     private HttpStatus status = HttpStatus.INTERNAL_SERVER_ERROR;
     public CityInvalidException() {
-        super("지역이 유요하지 않습니다.");
+        super("지역이 유효하지 않습니다.");
     }
 
     public HttpStatus getStatus() {

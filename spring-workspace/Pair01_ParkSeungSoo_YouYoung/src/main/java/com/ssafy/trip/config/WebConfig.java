07de595794064a0aa30f026f4cf315cfc62c229a
유영 <dbdud1999@naver.com>
2023-09-28 22:08:06 +0900

    :lock: CORS 에러 해결

--- spring-workspace/Pair01_ParkSeungSoo_YouYoung/src/main/java/com/ssafy/trip/config/WebConfig.java
+++ spring-workspace/Pair01_ParkSeungSoo_YouYoung/src/main/java/com/ssafy/trip/config/WebConfig.java
@@ -11,9 +11,10 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:8080")
+                .allowedOrigins("http://localhost:3000")
                 .allowCredentials(true)
-                .allowedMethods("GET", "POST");
+                .allowedMethods("GET", "POST", "OPTIONS")
+                .allowedHeaders("*");
     }
 }
 

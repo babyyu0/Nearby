32a7546d52db1206ebfd11dbfc7340cf96a5b6d2
유영 <dbdud1999@naver.com>
2023-09-29 22:01:58 +0900

    :recycle: 코드 위치 변경

--- spring-workspace/Pair01_ParkSeungSoo_YouYoung/src/main/java/com/ssafy/trip/util/exception/MyException.java
+++ spring-workspace/Pair01_ParkSeungSoo_YouYoung/src/main/java/com/ssafy/trip/util/exception/MyException.java
@@ -3,8 +3,8 @@ package com.ssafy.trip.util.exception;
 import org.springframework.http.HttpStatus;
 
 public abstract class MyException extends Exception{
-	abstract public HttpStatus getStatus();
 	public MyException(String message) {
 		super(message);
 	}
+	abstract public HttpStatus getStatus();
 }

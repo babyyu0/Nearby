2fe91ba07807d2719cef483221ff54a7ffb4ecee
유영 <dbdud1999@naver.com>
2023-09-30 23:58:20 +0900

    :lock: 비밀번호 중복 확인 처리

---
+++ spring-workspace/Pair01_ParkSeungSoo_YouYoung/src/main/java/com/ssafy/trip/util/exception/common/MessageInvalidException.java
@@ -0,0 +1,16 @@
+package com.ssafy.trip.util.exception.common;
+
+import com.ssafy.trip.util.exception.MyException;
+import org.springframework.http.HttpStatus;
+
+public class MessageInvalidException extends MyException {
+
+    private HttpStatus status = HttpStatus.INTERNAL_SERVER_ERROR;
+    public MessageInvalidException() {
+        super("메시지가 유효하지 않습니다.");
+    }
+
+    public HttpStatus getStatus() {
+        return status;
+    }
+}

b7af28b8b00b022574142e55255ea0e9d75540a2
You Young <dbdud1999@naver.com>
2023-10-11 17:38:56 +0900

    :art: 도시 코드 없을 경우 불러오기

--- spring-workspace/Pair01_ParkSeungSoo_YouYoung/src/main/java/com/ssafy/trip/model/service/TripServiceImpl.java
+++ spring-workspace/Pair01_ParkSeungSoo_YouYoung/src/main/java/com/ssafy/trip/model/service/TripServiceImpl.java
@@ -1,14 +1,14 @@
 package com.ssafy.trip.model.service;
 
-import com.ssafy.trip.model.dao.GugunDao;
-import com.ssafy.trip.model.dao.SidoDao;
 import com.ssafy.trip.model.dto.response.CityResponse;
 import com.ssafy.trip.model.dto.response.GugunResponse;
 import com.ssafy.trip.model.dto.response.SidoResponse;
-import com.ssafy.trip.model.vo.Gugun;
-import com.ssafy.trip.model.vo.Sido;
+import com.ssafy.trip.model.entity.Gugun;
+import com.ssafy.trip.model.entity.Sido;
+import com.ssafy.trip.model.repository.GugunRepository;
+import com.ssafy.trip.model.repository.SidoRepository;
+import com.ssafy.trip.model.service.common.TripApiService;
 import com.ssafy.trip.util.exception.MyException;
-import com.ssafy.trip.util.exception.common.CityInvalidException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -20,30 +20,33 @@ import java.util.List;
 @Slf4j
 public class TripServiceImpl implements TripService {
 
-    private final SidoDao sidoDao;
-    private final GugunDao gugunDao;
+    private final TripApiService tripApiService;
+    private final SidoRepository sidoRepository;
+    private final GugunRepository gugunRepository;
 
     @Autowired
-    public TripServiceImpl(SidoDao sidoDao, GugunDao gugunDao) {
-        this.sidoDao = sidoDao;
-        this.gugunDao = gugunDao;
+    public TripServiceImpl(TripApiService tripApiService, SidoRepository sidoRepository, GugunRepository gugunRepository) {
+        this.tripApiService = tripApiService;
+        this.sidoRepository = sidoRepository;
+        this.gugunRepository = gugunRepository;
     }
 
     public CityResponse getCity() throws MyException {
-        List<Sido> sidoList = sidoDao.selectAll();
-        System.out.println(1);
-        List<Gugun> gugunList = gugunDao.selectAll();
-        System.out.println(2);
+        List<Sido> sidoList = sidoRepository.findAll();
+        List<Gugun> gugunList = gugunRepository.findAll();
 
         List<SidoResponse> sidoResponseList = new ArrayList<>();
         List<GugunResponse> gugunResponseList = new ArrayList<>();
 
+        if(sidoList.isEmpty()) {
+            tripApiService.getCityCode();
+        }
         for (Sido sido : sidoList) {
             sidoResponseList.add(new SidoResponse(sido.getSidoCode(), sido.getSidoName()));
         }
 
         for (Gugun gugun : gugunList) {
-            gugunResponseList.add(new GugunResponse(gugun.getGugunCode(), gugun.getGugunName(), gugun.getSidoCode()));
+            gugunResponseList.add(new GugunResponse(gugun.getGugunCode(), gugun.getGugunName(), gugun.getSido().getSidoCode()));
         }
 
         return new CityResponse(sidoResponseList, gugunResponseList);

20a84c62d3ac7bda861453a4ca19021daf4faefd
유영 <dbdud1999@naver.com>
2023-10-13 00:30:15 +0900

    :bug: 복합키 생성 방식 변경

--- spring-workspace/Pair01_ParkSeungSoo_YouYoung/src/main/java/com/ssafy/trip/model/data/GugunPk.java
+++ spring-workspace/Pair01_ParkSeungSoo_YouYoung/src/main/java/com/ssafy/trip/model/data/GugunPk.java
@@ -1,40 +1,31 @@
 package com.ssafy.trip.model.data;
 
 import com.ssafy.trip.model.entity.Sido;
-import com.ssafy.trip.util.exception.common.CityInvalidException;
-import jakarta.persistence.Embeddable;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.PrimaryKeyJoinColumn;
-import lombok.Data;
+import jakarta.persistence.*;
+import lombok.*;
 import lombok.extern.slf4j.Slf4j;
-import org.hibernate.annotations.Comment;
 
 import java.io.Serializable;
 
 @Embeddable
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@Builder(builderMethodName = "InnerBuilder")
 @Slf4j
+@ToString
 public class GugunPk implements Serializable {
     private long gugunCode;
     private Sido sido;
 
-    public long getGugunCode() {
-        return gugunCode;
+    public static GugunPkBuilder builder() {
+        return GugunPk.InnerBuilder();
     }
 
-    public void setGugunCode(long gugunCode) throws CityInvalidException {
-        if(gugunCode <= 0) {
-            log.error("Gugun: 구군 코드 받기 실패");
-            throw new CityInvalidException();
-        }
-        this.gugunCode = gugunCode;
+    public GugunPkBuilder gugunCode(long gugunCode) {
+        return InnerBuilder().gugunCode(gugunCode);
     }
 
-    public Sido getSido() {
-        return sido;
-    }
-
-    public void setSido(Sido sido) {
-        this.sido = sido;
+    public GugunPkBuilder sido(Sido sido) {
+        return InnerBuilder().sido(sido);
     }
 }

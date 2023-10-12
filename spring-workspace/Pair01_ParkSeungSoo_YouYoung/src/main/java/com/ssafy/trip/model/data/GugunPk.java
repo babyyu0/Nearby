bd37c185a3a380b1644e0539962b08cdbb7ccc50
You Young <dbdud1999@naver.com>
2023-10-12 18:15:58 +0900

    :bug: PK 두개 설정

--- spring-workspace/Pair01_ParkSeungSoo_YouYoung/src/main/java/com/ssafy/trip/model/data/GugunPk.java
+++ spring-workspace/Pair01_ParkSeungSoo_YouYoung/src/main/java/com/ssafy/trip/model/data/GugunPk.java
@@ -1,12 +1,40 @@
 package com.ssafy.trip.model.data;
 
 import com.ssafy.trip.model.entity.Sido;
+import com.ssafy.trip.util.exception.common.CityInvalidException;
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.PrimaryKeyJoinColumn;
 import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import org.hibernate.annotations.Comment;
 
 import java.io.Serializable;
 
-@Data
+@Embeddable
+@Slf4j
 public class GugunPk implements Serializable {
     private long gugunCode;
     private Sido sido;
+
+    public long getGugunCode() {
+        return gugunCode;
+    }
+
+    public void setGugunCode(long gugunCode) throws CityInvalidException {
+        if(gugunCode <= 0) {
+            log.error("Gugun: 구군 코드 받기 실패");
+            throw new CityInvalidException();
+        }
+        this.gugunCode = gugunCode;
+    }
+
+    public Sido getSido() {
+        return sido;
+    }
+
+    public void setSido(Sido sido) {
+        this.sido = sido;
+    }
 }

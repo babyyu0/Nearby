a73a025244fd76acf4f601ddc4cbedc5f44e47de
You Young <dbdud1999@naver.com>
2023-10-12 18:13:59 +0900

    :bug: int형 아이디 long으로 변경

--- spring-workspace/Pair01_ParkSeungSoo_YouYoung/src/main/java/com/ssafy/trip/model/entity/Sido.java
+++ spring-workspace/Pair01_ParkSeungSoo_YouYoung/src/main/java/com/ssafy/trip/model/entity/Sido.java
@@ -10,40 +10,35 @@ import org.hibernate.annotations.Comment;
 @Entity
 @NoArgsConstructor
 public class Sido {
-	@Builder
-	public Sido(int sidoCode, String sidoName) {
-		setSidoCode(sidoCode);
-		setSidoName(sidoName);
-	}
+    @Id
+    @Column(name = "sido_code")
+    @Comment("코드")
+    private long sidoCode;
 
-	@Id
-	@Column(name="sido_code")
-	@Comment("코드")
-	private int sidoCode;
+    @Column(name = "sido_name", nullable = false, columnDefinition = "VARCHAR(20) CHARACTER SET UTF8")
+    @Comment("이름")
+    private String sidoName;
 
-	@Column(name = "sido_name", nullable = false, columnDefinition = "VARCHAR(20) CHARACTER SET UTF8")
-	@Comment("이름")
-	private String sidoName;
+    @Builder
+    public Sido(long sidoCode, String sidoName) {
+        setSidoCode(sidoCode);
+        setSidoName(sidoName);
+    }
 
-	public int getSidoCode() {
-		return sidoCode;
-	}
+    public long getSidoCode() {
+        return sidoCode;
+    }
 
-	public void setSidoCode(int sidoCode) {
-		this.sidoCode = sidoCode;
-	}
+    public void setSidoCode(long sidoCode) {
+        this.sidoCode = sidoCode;
+    }
 
-	public String getSidoName() {
-		return sidoName;
-	}
+    public String getSidoName() {
+        return sidoName;
+    }
 
-	public void setSidoName(String sidoName) {
-		this.sidoName = sidoName;
-	}
+    public void setSidoName(String sidoName) {
+        this.sidoName = sidoName;
+    }
 
-	@Override
-	public String toString() {
-		return "Sido [sidoCode=" + sidoCode + ", sidoName=" + sidoName + "]";
-	}
-	
 }

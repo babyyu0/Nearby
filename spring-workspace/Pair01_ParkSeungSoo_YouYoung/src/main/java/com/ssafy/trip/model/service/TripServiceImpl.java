3875ba42e06ce3027a73ee38249428224124a69d
유영 <dbdud1999@naver.com>
2023-10-24 00:18:49 +0900

    :fire: Api Service 의존성 제거

--- spring-workspace/Pair01_ParkSeungSoo_YouYoung/src/main/java/com/ssafy/trip/model/service/TripServiceImpl.java
+++ spring-workspace/Pair01_ParkSeungSoo_YouYoung/src/main/java/com/ssafy/trip/model/service/TripServiceImpl.java
@@ -7,8 +7,8 @@ import com.ssafy.trip.model.entity.Gugun;
 import com.ssafy.trip.model.entity.Sido;
 import com.ssafy.trip.model.repository.GugunRepository;
 import com.ssafy.trip.model.repository.SidoRepository;
-import com.ssafy.trip.model.service.common.TripApiService;
 import com.ssafy.trip.util.exception.MyException;
+import com.ssafy.trip.util.exception.trip.CityInvalidException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -20,13 +20,11 @@ import java.util.List;
 @Slf4j
 public class TripServiceImpl implements TripService {
 
-    private final TripApiService tripApiService;
     private final SidoRepository sidoRepository;
     private final GugunRepository gugunRepository;
 
     @Autowired
-    public TripServiceImpl(TripApiService tripApiService, SidoRepository sidoRepository, GugunRepository gugunRepository) {
-        this.tripApiService = tripApiService;
+    public TripServiceImpl(SidoRepository sidoRepository, GugunRepository gugunRepository) {
         this.sidoRepository = sidoRepository;
         this.gugunRepository = gugunRepository;
     }
@@ -39,12 +37,10 @@ public class TripServiceImpl implements TripService {
         List<GugunResponse> gugunResponseList = new ArrayList<>();
 
         if(sidoList.isEmpty()) {
-            tripApiService.getSidoCode();
-            sidoList = sidoRepository.findAll();
+            throw new CityInvalidException();
         }
         if(gugunList.isEmpty()) {
-            tripApiService.getGugunCode(sidoList);
-            gugunList = gugunRepository.findAll();
+            throw new CityInvalidException();
         }
         for (Sido sido : sidoList) {
             sidoResponseList.add(SidoResponse.builder().sidoCode(sido.getSidoCode()).sidoName(sido.getSidoName()).build());

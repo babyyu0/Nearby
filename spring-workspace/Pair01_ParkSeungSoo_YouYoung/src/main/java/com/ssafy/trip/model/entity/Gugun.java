cdbdabbff12b46ed4e88e43f87a22a35469cb1d4
유영 <dbdud1999@naver.com>
2023-11-20 23:02:44 +0900

    :art: InnerBuilder 제거

--- spring-workspace/Pair01_ParkSeungSoo_YouYoung/src/main/java/com/ssafy/trip/model/entity/Gugun.java
+++ spring-workspace/Pair01_ParkSeungSoo_YouYoung/src/main/java/com/ssafy/trip/model/entity/Gugun.java
@@ -10,7 +10,7 @@ import org.hibernate.annotations.Comment;
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor(access = AccessLevel.PROTECTED)
-@Builder(builderMethodName = "innerBuilder")
+@Builder
 @IdClass(GugunPk.class)
 @Getter
 @Slf4j
@@ -19,40 +19,15 @@ public class Gugun {
 	@Id
 	@Column(name = "gugun_code")
 	@Comment("코드")
-	private long gugunCode;
+	private int gugunCode;
 
 	@Column(name = "gugun_name", nullable = false, columnDefinition = "VARCHAR(20) CHARACTER SET UTF8")
-	@Comment("이름")
+	@Comment("구군명")
 	private String gugunName;
 
 	@ManyToOne(fetch = FetchType.LAZY)
 	@PrimaryKeyJoinColumn
 	@JoinColumn(name = "sido_code")
+	@Comment("시도 코드")
 	private Sido sido;
-
-	public static GugunBuilder builder() {
-		return Gugun.innerBuilder();
-	}
-
-	public GugunBuilder gugunCode(long gugunCode) throws CityInvalidException {
-		if(gugunCode <= 0) {
-			log.error("Gugun: 구군 코드 입력 실패 " + gugunCode);
-			throw new CityInvalidException();
-		}
-		return innerBuilder().gugunCode(gugunCode);
-	}
-
-	public GugunBuilder gugunName(String gugunName) throws CityInvalidException {
-		if(gugunName == null || gugunName.trim().equals("")) {
-			log.error("Gugun: 지역 (구, 군) 이름 입력 실패 " + gugunName);
-			throw new CityInvalidException();
-		}
-		return innerBuilder().gugunName(gugunName);
-	}
-
-	public GugunBuilder sido(Sido sido) {
-		this.sido = sido;
-		return innerBuilder().sido(sido);
-	}
-
 }

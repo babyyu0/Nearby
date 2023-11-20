89f693e733d05ddca4d88e62ef74235f67456e0f
You Young <dbdud1999@naver.com>
2023-11-20 18:03:45 +0900

    :lock: 예외 추가

--- spring-workspace/Pair01_ParkSeungSoo_YouYoung/src/main/java/com/ssafy/trip/util/exception/ErrorMessage.java
+++ spring-workspace/Pair01_ParkSeungSoo_YouYoung/src/main/java/com/ssafy/trip/util/exception/ErrorMessage.java
@@ -3,12 +3,13 @@ package com.ssafy.trip.util.exception;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import static org.springframework.http.HttpStatus.*;
 
 @Getter
 @RequiredArgsConstructor
 public enum ErrorMessage {
-    ;
-    
-    private final HttpStatus httpStatus;
+    SIDO_INVALID(INTERNAL_SERVER_ERROR, "시도가 유효하지 않습니다.");
+
+    private final HttpStatus status;
     private final String message;
 }

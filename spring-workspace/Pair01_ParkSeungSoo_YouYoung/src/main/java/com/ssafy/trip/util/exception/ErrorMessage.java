891c9bdeb0002f89b630dd5e66be912ac56351be
유영 <dbdud1999@naver.com>
2023-11-20 22:59:56 +0900

    :lock: 구군 유효성 오류 추가

--- spring-workspace/Pair01_ParkSeungSoo_YouYoung/src/main/java/com/ssafy/trip/util/exception/ErrorMessage.java
+++ spring-workspace/Pair01_ParkSeungSoo_YouYoung/src/main/java/com/ssafy/trip/util/exception/ErrorMessage.java
@@ -8,7 +8,8 @@ import static org.springframework.http.HttpStatus.*;
 @Getter
 @RequiredArgsConstructor
 public enum ErrorMessage {
-    SIDO_INVALID(INTERNAL_SERVER_ERROR, "시도가 유효하지 않습니다.");
+    SIDO_INVALID(INTERNAL_SERVER_ERROR, "시도가 유효하지 않습니다."),
+    GUGUN_INVALID(INTERNAL_SERVER_ERROR, "구군이 유효하지 않습니다.");
 
     private final HttpStatus status;
     private final String message;

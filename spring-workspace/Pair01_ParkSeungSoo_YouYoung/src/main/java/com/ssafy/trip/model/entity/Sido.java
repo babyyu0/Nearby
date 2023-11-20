4084100cf87894c86504eea833e731b17ed500c5
You Young <dbdud1999@naver.com>
2023-11-20 18:01:32 +0900

    :art: sidoCode long -> int 변경 및 innerBuilder 삭제

--- spring-workspace/Pair01_ParkSeungSoo_YouYoung/src/main/java/com/ssafy/trip/model/entity/Sido.java
+++ spring-workspace/Pair01_ParkSeungSoo_YouYoung/src/main/java/com/ssafy/trip/model/entity/Sido.java
@@ -1,7 +1,5 @@
 package com.ssafy.trip.model.entity;
 
-import com.ssafy.trip.util.data.RegexData;
-import com.ssafy.trip.util.exception.member.MemberInvalidException;
 import com.ssafy.trip.util.exception.trip.CityInvalidException;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -13,35 +11,16 @@ import org.hibernate.annotations.Comment;
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor(access = AccessLevel.PROTECTED)
-@Builder(builderMethodName = "innerBuilder")
+@Builder
 @Getter
 @Slf4j
 public class Sido {
     @Id
     @Column(name = "sido_code")
     @Comment("코드")
-    private long sidoCode;
+    private int sidoCode;
 
     @Column(name = "sido_name", nullable = false, columnDefinition = "VARCHAR(20) CHARACTER SET UTF8")
     @Comment("이름")
     private String sidoName;
-
-    public static SidoBuilder builder() {
-        return Sido.innerBuilder();
-    }
-
-    public SidoBuilder sidoCode(long sidoCode) throws CityInvalidException {
-        if(sidoCode <= 0) {
-            log.error("Sido: 지역 (시, 도) 번호 입력 실패 " + sidoCode);
-            throw new CityInvalidException();
-        }
-        return innerBuilder().sidoCode(sidoCode);
-    }
-    public SidoBuilder sidoName(String sidoName) throws CityInvalidException {
-        if(sidoName == null || sidoName.trim().equals("")) {
-            log.error("Sido: 지역 (시, 도) 입력 실패 " + sidoName);
-            throw new CityInvalidException();
-        }
-        return innerBuilder().sidoName(sidoName);
-    }
 }

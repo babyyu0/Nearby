db3c9e3346c946e7547d62fa8bff80a4c75627d9
You Young <dbdud1999@naver.com>
2023-11-21 12:31:52 +0900

    :art: 정규식 정의 방식 및 인터페이스명 변경

---
+++ spring-workspace/Pair01_ParkSeungSoo_YouYoung/src/main/java/com/ssafy/trip/util/data/RegexPattern.java
@@ -0,0 +1,6 @@
+package com.ssafy.trip.util.data;
+
+public interface RegexPattern {
+    String EMAIL = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,20}$";
+    String PASSWORD = "^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{6,20}$";
+}

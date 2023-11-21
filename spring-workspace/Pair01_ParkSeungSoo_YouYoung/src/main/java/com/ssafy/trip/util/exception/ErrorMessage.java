a583960b1607c70a15f20f1be63888083a51669b
유영 <dbdud1999@naver.com>
2023-11-21 22:08:06 +0900

    :lock: 토큰 관련 예외 메시지 추가

--- spring-workspace/Pair01_ParkSeungSoo_YouYoung/src/main/java/com/ssafy/trip/util/exception/ErrorMessage.java
+++ spring-workspace/Pair01_ParkSeungSoo_YouYoung/src/main/java/com/ssafy/trip/util/exception/ErrorMessage.java
@@ -8,6 +8,8 @@ import static org.springframework.http.HttpStatus.*;
 @Getter
 @RequiredArgsConstructor
 public enum ErrorMessage {
+    TOKEN_NOT_FOUND(BAD_REQUEST, "회원을 찾을 수 없습니다."),
+    TOKEN_EXPIRED(BAD_REQUEST, "로그인이 만료 되었습니다."),
     IMAGE_NOT_FOUND(BAD_REQUEST, "이미지를 찾을 수 없습니다"),
     IMAGE_INVALID(BAD_REQUEST, "이미지가 유효하지 않습니다"),
     ID_DUPLICATED(BAD_REQUEST, "중복된 아이디 입니다."),

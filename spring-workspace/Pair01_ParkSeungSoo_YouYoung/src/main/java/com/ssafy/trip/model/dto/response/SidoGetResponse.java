15fc4f0d0e1cd34c53564ba1edc94a64cb9c6185
You Young <dbdud1999@naver.com>
2023-11-21 12:19:59 +0900

    :art: SidoGetResponse Builder 메소드 선언으로 변경

--- spring-workspace/Pair01_ParkSeungSoo_YouYoung/src/main/java/com/ssafy/trip/model/dto/response/SidoGetResponse.java
+++ spring-workspace/Pair01_ParkSeungSoo_YouYoung/src/main/java/com/ssafy/trip/model/dto/response/SidoGetResponse.java
@@ -6,13 +6,19 @@ import jakarta.validation.constraints.Positive;
 import lombok.AccessLevel;
 import lombok.Builder;
 
-@Builder(access = AccessLevel.PRIVATE)
+
 public record SidoGetResponse(
-        @Positive(message = "시도 코드는 자연수여야 합니다.")
+        @Positive(message = "지역 (시, 도) 코드는 자연수여야 합니다.")
         int code,
-        @NotBlank(message = "시도명이 존재하지 않습니다.")
+        @NotBlank(message = "지역 (시, 도) 이름이 존재하지 않습니다.")
         String name
 ) {
+    @Builder(access = AccessLevel.PRIVATE)
+    public SidoGetResponse(int code, String name) {
+        this.code = code;
+        this.name = name;
+    }
+
     public static SidoGetResponse from(Sido sido) {
         return SidoGetResponse.builder()
                 .code(sido.getSidoCode())

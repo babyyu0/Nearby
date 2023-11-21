ef8a09eac18b4cb02a0e5d2d314cb4e054a95178
유영 <dbdud1999@naver.com>
2023-11-21 21:25:42 +0900

    :lock: 예외 상황 추가

--- spring-workspace/Pair01_ParkSeungSoo_YouYoung/src/main/java/com/ssafy/trip/util/exception/ErrorMessage.java
+++ spring-workspace/Pair01_ParkSeungSoo_YouYoung/src/main/java/com/ssafy/trip/util/exception/ErrorMessage.java
@@ -8,8 +8,14 @@ import static org.springframework.http.HttpStatus.*;
 @Getter
 @RequiredArgsConstructor
 public enum ErrorMessage {
-    SIDO_INVALID(INTERNAL_SERVER_ERROR, "시도가 유효하지 않습니다."),
-    GUGUN_INVALID(INTERNAL_SERVER_ERROR, "구군이 유효하지 않습니다.");
+    IMAGE_NOT_FOUND(BAD_REQUEST, "이미지를 찾을 수 없습니다"),
+    IMAGE_INVALID(BAD_REQUEST, "이미지가 유효하지 않습니다"),
+    ID_DUPLICATED(BAD_REQUEST, "중복된 아이디 입니다."),
+    MEMBER_NOT_FOUND(BAD_REQUEST, "회원을 찾을 수 없습니다."),
+    SIDO_NOT_FOUND(BAD_REQUEST, "지역(시, 도)을 찾을 수 없습니다"),
+    GUGUN_NOT_FOUND(BAD_REQUEST, "지역(구, 군)을 찾을 수 없습니다"),
+    SIDO_INVALID(INTERNAL_SERVER_ERROR, "지역(시, 도)이 유효하지 않습니다."),
+    GUGUN_INVALID(INTERNAL_SERVER_ERROR, "지역(구, 군)이 유효하지 않습니다.");
 
     private final HttpStatus status;
     private final String message;

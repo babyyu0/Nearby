6b51281cb2236a17cb21c63005b3661fc294dc20
You Young <dbdud1999@naver.com>
2023-11-21 14:34:25 +0900

    :fire: Image 작업 Util화에 따른 Service 삭제

--- spring-workspace/Pair01_ParkSeungSoo_YouYoung/src/main/java/com/ssafy/trip/model/service/common/ImageService.java
+++
@@ -1,7 +0,0 @@
-package com.ssafy.trip.model.service.common;
-
-import com.ssafy.trip.util.exception.common.FileNotFoundException;
-
-public interface ImageService {
-    byte[] imgToByteArray(String url, String imgName) throws FileNotFoundException;
-}

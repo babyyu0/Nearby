b4efa1994d326b4ae46c41c29719df00c9b3b9a2
You Young <dbdud1999@naver.com>
2023-11-21 12:19:21 +0900

    :art: GugunResponse 이름 변경 및 Record 수정

---
+++ spring-workspace/Pair01_ParkSeungSoo_YouYoung/src/main/java/com/ssafy/trip/model/dto/response/GugunGetResponse.java
@@ -0,0 +1,31 @@
+package com.ssafy.trip.model.dto.response;
+
+import com.ssafy.trip.model.entity.Gugun;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+import lombok.AccessLevel;
+import lombok.Builder;
+
+public record GugunGetResponse(
+        @Positive(message = "지역 (구, 군) 코드는 자연수여야 합니다.")
+        int code,
+        @NotBlank(message = "지역 (구, 군) 이름이 존재하지 않습니다.")
+        String name,
+        @Positive(message = "지역 (시, 도) 코드는 자연수여야 합니다.")
+        int sidoCode
+) {
+        @Builder(access = AccessLevel.PRIVATE)
+        public GugunGetResponse(int code, String name, int sidoCode) {
+                this.code = code;
+                this.name = name;
+                this.sidoCode = sidoCode;
+        }
+
+        public static GugunGetResponse from(Gugun gugun) {
+                return GugunGetResponse.builder()
+                        .code(gugun.getGugunCode())
+                        .name(gugun.getGugunName())
+                        .sidoCode(gugun.getSido().getSidoCode())
+                        .build();
+        }
+}

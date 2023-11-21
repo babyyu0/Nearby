6dbac05112438c6c5367b35a10f285ecbfbfc4b4
You Young <dbdud1999@naver.com>
2023-11-21 14:32:52 +0900

    :art: Login API 수정

--- spring-workspace/Pair01_ParkSeungSoo_YouYoung/src/main/java/com/ssafy/trip/model/service/MemberService.java
+++ spring-workspace/Pair01_ParkSeungSoo_YouYoung/src/main/java/com/ssafy/trip/model/service/MemberService.java
@@ -1,14 +1,17 @@
 package com.ssafy.trip.model.service;
 
+import com.ssafy.trip.model.dto.command.LoginCommandDto;
 import com.ssafy.trip.model.dto.command.RegisterCommandDto;
 import com.ssafy.trip.model.dto.command.ValidIdCommandDto;
+import com.ssafy.trip.model.dto.response.LoginResponseDto;
 import com.ssafy.trip.model.dto.response.ValidIdResponseDto;
 import com.ssafy.trip.util.exception.MyException;
 
 public interface MemberService {
+
     ValidIdResponseDto isValidId(ValidIdCommandDto validIdCommandDto) throws MyException;
+
     boolean register(RegisterCommandDto registerCommandDto) throws MyException;
-    // MemberLoginResponse login(MemberLoginCommand memberLoginCommand) throws MyException;
-    // MemberVO selectOne(MemberVO member);
-    // void setProfileImg(String id, File profileFile);
+
+    LoginResponseDto login(LoginCommandDto loginCommandDto) throws MyException;
 }
--- spring-workspace/Pair01_ParkSeungSoo_YouYoung/src/main/java/com/ssafy/trip/model/service/MemberServiceImpl.java
+++ spring-workspace/Pair01_ParkSeungSoo_YouYoung/src/main/java/com/ssafy/trip/model/service/MemberServiceImpl.java
@@ -1,8 +1,10 @@
 package com.ssafy.trip.model.service;
 
 import com.ssafy.trip.model.data.GugunPk;
+import com.ssafy.trip.model.dto.command.LoginCommandDto;
 import com.ssafy.trip.model.dto.command.RegisterCommandDto;
 import com.ssafy.trip.model.dto.command.ValidIdCommandDto;
+import com.ssafy.trip.model.dto.response.LoginResponseDto;
 import com.ssafy.trip.model.dto.response.ValidIdResponseDto;
 import com.ssafy.trip.model.entity.Gugun;
 import com.ssafy.trip.model.entity.Member;
@@ -17,6 +19,7 @@ import com.ssafy.trip.util.exception.ErrorMessage;
 import com.ssafy.trip.util.exception.MyException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -25,6 +28,9 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 @Slf4j
 public class MemberServiceImpl implements MemberService {
+
+    @Value("${member.profile.img.url}")
+    private static String MEMBER_PROFILE_IMG_URI;
     private final PasswordEncoder passwordEncoder;
     private final MemberRepository memberRepository;
     private final SidoRepository sidoRepository;
@@ -33,13 +39,22 @@ public class MemberServiceImpl implements MemberService {
     @Override
     @Transactional(readOnly = true)
     public ValidIdResponseDto isValidId(ValidIdCommandDto validIdCommandDto) throws MyException {
-        ValidIdResponseDto validIdResponseDto = ValidIdResponseDto.builder().valid(true).message("사용 가능한 아이디 입니다.").build();
+        ValidIdResponseDto validIdResponseDto = ValidIdResponseDto.builder()
+                .valid(true)
+                .message("사용 가능한 아이디 입니다.")
+                .build();
+
         if (validIdCommandDto.memberId() == null
                 || validIdCommandDto.memberId().trim().equals("")
                 || !validIdCommandDto.memberId().trim().matches(RegexPattern.EMAIL)) {
-            validIdResponseDto = ValidIdResponseDto.builder().valid(false).message("아이디 형식이 올바르지 않습니다.").build();
+            validIdResponseDto = ValidIdResponseDto.builder()
+                    .valid(false)
+                    .message("아이디 형식이 올바르지 않습니다.").build();
         } else if (memberRepository.findByMemberId(validIdCommandDto.memberId()).isPresent()) {
-            validIdResponseDto = ValidIdResponseDto.builder().valid(false).message("중복된 아이디입니다.").build();
+            validIdResponseDto = ValidIdResponseDto.builder()
+                    .valid(false)
+                    .message("중복된 아이디입니다.")
+                    .build();
         }
 
         return validIdResponseDto;
@@ -55,7 +70,7 @@ public class MemberServiceImpl implements MemberService {
 
         String profileName = null;
         if (!registerCommandDto.profile().isEmpty()) {  // 프로필 사진 존재할 경우
-            ImageUtil.saveMemberProfile(registerCommandDto.profile());
+            profileName = ImageUtil.saveIamge(MEMBER_PROFILE_IMG_URI, registerCommandDto.profile());
         }
 
         Sido sido = sidoRepository.findById(registerCommandDto.sidoCode())
@@ -73,107 +88,27 @@ public class MemberServiceImpl implements MemberService {
                 .profileImg(profileName)
                 .build();
 
-        ValidateUtil.serverValidate(member);
+        ValidateUtil.serverValidate(member);  // 유효성 검사
         memberRepository.save(member);
 
         return true;
     }
 
-/*
-    public MemberLoginResponse login(MemberLoginCommand memberLoginCommand) throws MyException {
-        Member member = memberRepository.findByMemberId(memberLoginCommand.memberId()).orElseThrow(() -> {
-            log.error("MemberService: 회원 아이디 찾기 실패 (Member) " + memberLoginCommand.memberId());
-            return new MemberNotFoundException();
-        });
+    public LoginResponseDto login(LoginCommandDto loginCommandDto)
+            throws MyException {
+        ValidateUtil.clientValidate(loginCommandDto);  // 유효성 검사
+        Member member = memberRepository.findByMemberId(loginCommandDto.id())
+                .orElseThrow(() -> new MyException(ErrorMessage.MEMBER_NOT_FOUND));
 
-        if (!myPasswordEncoder.matches(memberLoginCommand.getPassword(), member.getPassword())) {
-            log.error("MemberService: 비밀번호 비교 실패 " + memberLoginCommand.getPassword());
-            throw new MemberNotFoundException();
+        // 비밀번호 틀렸을 시
+        if (!passwordEncoder.matches(loginCommandDto.password(), member.getPassword())) {
+            throw new MyException(ErrorMessage.MEMBER_NOT_FOUND);
         }
 
-        return MemberLoginResponse.builder()
-                .name(member.getName())
-                .profile(imageService.imgToByteArray(MEMBER_PROFILE_IMG_URI, member.getProfileImg()))
-                .build();
+        LoginResponseDto loginResponseDto = LoginResponseDto.from(member,
+                ImageUtil.toByteArray(MEMBER_PROFILE_IMG_URI, member.getProfileImg()));
+        ValidateUtil.serverValidate(loginResponseDto);  // 유효성 검사
+
+        return loginResponseDto;
     }
-
-}
-	public String login(MemberVO member) throws MyException {
-		try {
-			MemberSecVO loggedMemberSec = memberSecDAO.login(member);
-			if(loggedMemberSec == null) {
-				return null;
-			}
-			
-			String salt = loggedMemberSec.getSalt();
-			// 해쉬값 비교
-			String hashPassword = OpenCrypt.getSHA256(member.getPassword(), salt);
-			member.setPassword(hashPassword);
-			
-			String loggedMemberName = memberRepository.login(member);
-			// 아이디 혹은 비밀번호가 일치하지 않을 경우
-			if(loggedMemberName == null) {
-				return null;
-			}
-			
-			if(member.getPassword().equals(hashPassword)) {
-				return loggedMemberName;
-			}
-		} catch (Exception e) {
-			throw new RuntimeException("해쉬값 변환 실패.");
-		}
-
-		return null;
-	}
-
-	public MemberVO selectOne(MemberVO member) {
-		return memberRepository.selectOne(member);
-	}
-
-	public void regist(MemberVO member) throws MyException {
-
-		// 아이디 중복 검사
-		MemberVO selMemberVO = selectOne(member);
-		if(selMemberVO != null) throw new RuntimeException("회원 등록 실패.");
-		
-		// 비밀번호 유효성 검사
-		String pwReg = "^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[~!@#$%^&*_-])[A-Za-z0-9~!@#$%^&*_-]{8,16}$";
-		if(!Pattern.matches(pwReg, member.getPassword())) {
-			throw new RuntimeException("회원 등록 실패.");
-		}
-		
-		if(member.getName().equals("")) {
-			throw new RuntimeException("회원 등록 실패.");
-		}
-		
-		String emailReg = "^[a-zA-Z0-9+-\\_.]+@[a-zA-Z0-9-]+\\.[a-zA-Z0-9-.]+$";
-		if(!Pattern.matches(emailReg, member.getEmail())) {
-			throw new RuntimeException("회원 등록 실패.");
-		}
-		
-		try {
-			//회원가입 시 입력한 비밀번호를 암호화 하기 위해 렌덤해시값인 salt를 UUID.randomUUID()을 통해서 생성합니다.
-			String salt = UUID.randomUUID().toString();
-			System.out.println("salt : " + salt);
-			// 사용자가 회원 가입 시 입력한 password와 salt값을 바탕으로 랜덤 해쉬값을 생성합니다.
-			String hashPassword = OpenCrypt.getSHA256(member.getPassword(), salt);
-			member.setPassword(hashPassword);
-			byte[]  secKey = OpenCrypt.generateKey("AES", 128);
-			
-			memberRepository.regist(member);
-			
-			// 암호화한 아이디를 저장합니다.
-			memberSecDAO.registSec(new MemberSecVO(member.getId(), salt, new String(secKey)));
-		} catch (Exception e) {
-			throw new RuntimeException("회원 등록 실패.");
-		}
-		
-	}
-	
-	public void setProfileImg(String id, File profileFile) {
-		memberRepository.insertProfileImg(id, "http://localhost:9999/static/img/userProfile/" + profileFile.getName());
-	}
-
- */
-
 }

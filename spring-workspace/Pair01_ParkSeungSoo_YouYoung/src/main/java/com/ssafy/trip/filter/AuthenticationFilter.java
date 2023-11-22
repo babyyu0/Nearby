8a588f91b78355db1d404018d7127a2f94a6d151
유영 <dbdud1999@naver.com>
2023-11-23 00:30:40 +0900

    :bug: Token null 처리

--- spring-workspace/Pair01_ParkSeungSoo_YouYoung/src/main/java/com/ssafy/trip/filter/AuthenticationFilter.java
+++ spring-workspace/Pair01_ParkSeungSoo_YouYoung/src/main/java/com/ssafy/trip/filter/AuthenticationFilter.java
@@ -1,45 +1,33 @@
 package com.ssafy.trip.filter;
 
-import com.ssafy.trip.util.MyPasswordEncoder;
 import com.ssafy.trip.util.TokenProvider;
-import com.ssafy.trip.util.exception.ErrorMessage;
 import com.ssafy.trip.util.exception.MyException;
-import com.ssafy.trip.util.exception.common.CookieInvalidException;
-import com.ssafy.trip.util.exception.common.PasswordEncodeException;
-import com.ssafy.trip.util.exception.member.MemberInvalidException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
-import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
-import javax.crypto.SecretKey;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 
 @Component
+@RequiredArgsConstructor
 @Slf4j
 public class AuthenticationFilter extends OncePerRequestFilter {
-
-    private final MyPasswordEncoder myPasswordEncoder;
     private final TokenProvider tokenProvider;
-
-    public AuthenticationFilter(MyPasswordEncoder myPasswordEncoder, TokenProvider tokenProvider) {
-        this.myPasswordEncoder = myPasswordEncoder;
-        this.tokenProvider = tokenProvider;
-    }
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         String token = tokenProvider.resolveToken(request);
 
         // permitAll일 경우 거치지 X
-        if (!token.isBlank() && tokenProvider.validateToken(token)) {
+        if (token != null && tokenProvider.validateToken(token)) {
             Authentication authentication = tokenProvider.getAuthentication(token);
             SecurityContextHolder.getContext().setAuthentication(authentication);
         }

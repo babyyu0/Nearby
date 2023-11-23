9d3e13a0aeb271422a092ead94665a80adcc7126
You Young <dbdud1999@naver.com>
2023-11-23 17:37:24 +0900

    :truck: 토큰 에러 문구 변경

--- spring-workspace/Pair01_ParkSeungSoo_YouYoung/src/main/java/com/ssafy/trip/global/util/exception/ErrorMessage.java
+++ spring-workspace/Pair01_ParkSeungSoo_YouYoung/src/main/java/com/ssafy/trip/global/util/exception/ErrorMessage.java
@@ -8,7 +8,7 @@ import static org.springframework.http.HttpStatus.*;
 @Getter
 @RequiredArgsConstructor
 public enum ErrorMessage {
-    TOKEN_NOT_FOUND(BAD_REQUEST, "회원을 찾을 수 없습니다."),
+    TOKEN_NOT_FOUND(BAD_REQUEST, "로그인 정보가 존재하지 않습니다."),
     TOKEN_EXPIRED(BAD_REQUEST, "로그인이 만료 되었습니다."),
     IMAGE_NOT_FOUND(BAD_REQUEST, "이미지를 찾을 수 없습니다"),
     IMAGE_INVALID(BAD_REQUEST, "이미지가 유효하지 않습니다"),

b0e3b0258b0d1eca013d4139d1cdab2e343e9ab5
You Young <dbdud1999@naver.com>
2023-11-23 17:37:09 +0900

    :lock: 헤더 미존재 예외 처리 추가

--- spring-workspace/Pair01_ParkSeungSoo_YouYoung/src/main/java/com/ssafy/trip/global/aop/ExceptionAdvice.java
+++ spring-workspace/Pair01_ParkSeungSoo_YouYoung/src/main/java/com/ssafy/trip/global/aop/ExceptionAdvice.java
@@ -5,6 +5,7 @@ import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingRequestHeaderException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -13,16 +14,22 @@ import com.ssafy.trip.global.util.exception.MyException;
 @RestControllerAdvice
 @Slf4j
 public class ExceptionAdvice {
-	
+
 	@ExceptionHandler(DataAccessException.class)
 	public ResponseEntity<?> handleException(DataAccessException e) {
 		log.error("DataAccessException : {}", e.getMessage());
 		return ResponseEntity.internalServerError()
 				.body("서버 오류입니다. 상황이 지속되면 문의 게시판을 이용해 주세요.");
 	}
+	@ExceptionHandler(MissingRequestHeaderException.class)
+	public ResponseEntity<?> handleException(MissingRequestHeaderException e) {
+		log.error("MissingRequestHeaderException : {}", e.getMessage());
+		return ResponseEntity.badRequest()
+				.body("로그인 정보가 존재하지 않습니다.");
+	}
 
 	@ExceptionHandler(MethodArgumentNotValidException.class)
-	protected ResponseEntity<?> methodArgumentNotValidExceptionHandle(MethodArgumentNotValidException e) {
+	protected ResponseEntity<?> handleException(MethodArgumentNotValidException e) {
 		log.error("methodArgumentNotValidException : {}", e.getFieldErrors().get(0).getDefaultMessage());
 		return ResponseEntity.badRequest()
 				.body(e.getFieldErrors().get(0).getDefaultMessage());

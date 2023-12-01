b1c21a6fb805bb6d24beebdb890f730bcb908e62
유영 <dbdud1999@naver.com>
2023-12-01 22:26:20 +0900

    :art: 유명 관광지 조회 기능 추가

--- spring-workspace/Pair01_ParkSeungSoo_YouYoung/src/main/java/com/ssafy/trip/attraction/model/dto/response/AttractionGetResponseDto.java
+++ spring-workspace/Pair01_ParkSeungSoo_YouYoung/src/main/java/com/ssafy/trip/attraction/model/dto/response/AttractionGetResponseDto.java
@@ -1,9 +1,26 @@
 package com.ssafy.trip.attraction.model.dto.response;
 
+import com.ssafy.trip.attraction.model.entity.Attraction;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.PositiveOrZero;
+import lombok.AccessLevel;
+import lombok.Builder;
+
 public record AttractionGetResponseDto(
-        String code,
-        String name,
+        @Positive(message = "관광지 고유 번호는 자연수여야 합니다.")
+        int code,
+        @NotBlank(message = "관광지명이 존재하지 않습니다.")
+        String title,
+        @PositiveOrZero(message = "거리는 양수여야 합니다.")
         double km,
         byte[] img
 ) {
+        @Builder
+        public AttractionGetResponseDto(int code, String title, double km, byte[] img) {
+                this.code = code;
+                this.title = title;
+                this.km = km;
+                this.img = img;
+        }
 }
--- spring-workspace/Pair01_ParkSeungSoo_YouYoung/src/main/java/com/ssafy/trip/attraction/model/service/AttractionServiceImpl.java
+++ spring-workspace/Pair01_ParkSeungSoo_YouYoung/src/main/java/com/ssafy/trip/attraction/model/service/AttractionServiceImpl.java
@@ -1,17 +1,49 @@
 package com.ssafy.trip.attraction.model.service;
 
+import com.querydsl.core.Tuple;
 import com.ssafy.trip.attraction.model.dto.response.AttractionGetResponseDto;
+import com.ssafy.trip.attraction.model.entity.Attraction;
+import com.ssafy.trip.attraction.model.repository.AttractionHeartRepository;
+import com.ssafy.trip.global.util.ImageUtil;
+import com.ssafy.trip.global.util.ValidateUtil;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class AttractionServiceImpl implements AttractionService  {
+
+    @Value("${url.attraction.img}")
+    private static String ATTRACTION_IMG_URI;
+    private final AttractionHeartRepository attractionHeartRepository;
     @Override
     public List<AttractionGetResponseDto> getPopularAttraction() {
+        List<Tuple> attractionHeartList = attractionHeartRepository.findAllByOrderByCountDesc();
 
-        return null;
+        List<AttractionGetResponseDto> attractionGetResponseDtoList = new ArrayList<>();
+
+        AttractionGetResponseDto attractionGetResponseDto;
+        Attraction attraction;
+        double km = 1;
+        for(Tuple attractionHeart : attractionHeartList) {
+            attraction = attractionHeart.get(0, Attraction.class);
+            km = 1;  // TO DO :: KM 계산하기
+
+            attractionGetResponseDto = AttractionGetResponseDto.builder()
+                    .code(attraction.getCode())
+                    .title(attraction.getTitle())
+                    .km(km)
+                    .img(ImageUtil.toByteArray(ATTRACTION_IMG_URI, attraction.getImg()))
+                    .build();
+            ValidateUtil.serverValidate(attractionGetResponseDto);
+
+            attractionGetResponseDtoList.add(attractionGetResponseDto);
+        }
+
+        return attractionGetResponseDtoList;
     }
 }

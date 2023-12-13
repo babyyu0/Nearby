9e981d2c1fbd9c87f2c4170ffb8c053fb5b6350e
유영 <dbdud1999@naver.com>
2023-12-13 22:20:47 +0900

    :bug: Extend class 및 컬럼 이름 수정

--- spring-workspace/Pair01_ParkSeungSoo_YouYoung/src/main/java/com/ssafy/trip/attraction/model/entity/Cat.java
+++ spring-workspace/Pair01_ParkSeungSoo_YouYoung/src/main/java/com/ssafy/trip/attraction/model/entity/Cat.java
@@ -1,22 +1,22 @@
 package com.ssafy.trip.attraction.model.entity;
 
+import com.ssafy.trip.global.model.entity.Base;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.PositiveOrZero;
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import org.hibernate.annotations.Comment;
 
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
-public class Cat extends BootstrapMethodError {
+@ToString
+public class Cat extends Base {
 
     @NotBlank(message = "분류 고유 번호가 존재하지 않습니다.")
-    @Column(name = "name", nullable = false, columnDefinition = "VARCHAR(10) CHARACTER SET UTF8")
+    @Column(name = "code", nullable = false, columnDefinition = "VARCHAR(10) CHARACTER SET UTF8")
     @Comment("고유 번호")
     @Id
     private String code;
@@ -30,4 +30,11 @@ public class Cat extends BootstrapMethodError {
     @Column(name = "depth", nullable = false, columnDefinition = "TINYINT UNSIGNED")
     @Comment("분류명")
     private int depth;
+
+    @Builder
+    public Cat(String code, String name, int depth) {
+        this.code = code;
+        this.name = name;
+        this.depth = depth;
+    }
 }

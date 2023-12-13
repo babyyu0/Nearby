e8c1bdea7ab2b8fe1e1a8dbd3abea896ab41ff21
유영 <dbdud1999@naver.com>
2023-12-13 22:21:50 +0900

    :art: Repository 수정

--- spring-workspace/Pair01_ParkSeungSoo_YouYoung/src/main/java/com/ssafy/trip/attraction/model/repository/AttractionRepository.java
+++ spring-workspace/Pair01_ParkSeungSoo_YouYoung/src/main/java/com/ssafy/trip/attraction/model/repository/AttractionRepository.java
@@ -1,8 +1,7 @@
 package com.ssafy.trip.attraction.model.repository;
 
 import com.ssafy.trip.attraction.model.entity.Attraction;
-import com.ssafy.trip.attraction.model.entity.AttractionInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AttractionInfoRepository extends JpaRepository<AttractionInfo, Integer> {
+public interface AttractionRepository extends JpaRepository<Attraction, Integer> {
 }

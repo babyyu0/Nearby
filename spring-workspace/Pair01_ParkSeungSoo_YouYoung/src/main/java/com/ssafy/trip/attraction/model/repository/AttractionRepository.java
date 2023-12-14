b2d6f5242030dcefad6cfa8ffcf94a7ceacf39a4
유영 <dbdud1999@naver.com>
2023-12-14 19:54:37 +0900

    :sparkles: 거리 계산 관련 Query 진행 중

--- spring-workspace/Pair01_ParkSeungSoo_YouYoung/src/main/java/com/ssafy/trip/attraction/model/repository/AttractionRepository.java
+++ spring-workspace/Pair01_ParkSeungSoo_YouYoung/src/main/java/com/ssafy/trip/attraction/model/repository/AttractionRepository.java
@@ -1,7 +1,8 @@
 package com.ssafy.trip.attraction.model.repository;
 
 import com.ssafy.trip.attraction.model.entity.Attraction;
+import com.ssafy.trip.attraction.model.repository.custom.AttractionCustomRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AttractionRepository extends JpaRepository<Attraction, Integer> {
+public interface AttractionRepository extends JpaRepository<Attraction, Integer>, AttractionCustomRepository {
 }

60aa30ae03bec402853c1ceb811dca1176ed49ee
유영 <dbdud1999@naver.com>
2023-12-18 18:56:13 +0900

    :card_file_box: 하트 수 계산 JPA 추가

--- spring-workspace/Pair01_ParkSeungSoo_YouYoung/src/main/java/com/ssafy/trip/attraction/model/repository/AttractionHeartRepository.java
+++ spring-workspace/Pair01_ParkSeungSoo_YouYoung/src/main/java/com/ssafy/trip/attraction/model/repository/AttractionHeartRepository.java
@@ -1,8 +1,10 @@
 package com.ssafy.trip.attraction.model.repository;
 
+import com.ssafy.trip.attraction.model.entity.Attraction;
 import com.ssafy.trip.attraction.model.entity.AttractionHeart;
 import com.ssafy.trip.attraction.model.repository.custom.AttractionHeartCustomRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AttractionHeartRepository extends JpaRepository<AttractionHeart, Integer>, AttractionHeartCustomRepository {
+    int countAllByAttraction(Attraction attraction);
 }

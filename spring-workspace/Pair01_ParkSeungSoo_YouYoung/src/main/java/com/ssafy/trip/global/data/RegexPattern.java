401532bf9268c96ed96ae2d3eb72206fb7f0326f
유영 <dbdud1999@naver.com>
2023-12-21 16:49:13 +0900

    :art: 전화번호 정규식 두 개 추가

--- spring-workspace/Pair01_ParkSeungSoo_YouYoung/src/main/java/com/ssafy/trip/global/data/RegexPattern.java
+++ spring-workspace/Pair01_ParkSeungSoo_YouYoung/src/main/java/com/ssafy/trip/global/data/RegexPattern.java
@@ -3,4 +3,6 @@ package com.ssafy.trip.global.data;
 public interface RegexPattern {
     String EMAIL = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,20}$";
     String PASSWORD = "^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{6,20}$";
+    String TEL = "^[0-9]{2,3}-[0-9]{3,4}-[0-9]{3,4}$";
+    String TEL2 = "^[0-9]*$";
 }

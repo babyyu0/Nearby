6e6a4a9d67f09e5aebad9bfe3bbd9340d8471639
유영 <dbdud1999@naver.com>
2023-12-23 19:50:26 +0900

    :sparkles: 메인 카드 컴포넌트 추가

--- front-workspace/src/components/CardComponent.jsx
+++ front-workspace/src/components/CardComponent.jsx
@@ -1,9 +1,15 @@
 
 
-function CardComponent() {
+function CardComponent({ mainStyle, item }) {
 
+  console.log(item);
   return (
     <>
+      <div className={mainStyle.cardContents}>
+        <img className={mainStyle.cardImg} src={item.img} alt={`${item.title} 이미지`} />
+        <h3 className={mainStyle.cardTitle}>{item.title}</h3>
+        <p className={mainStyle.cardContent}>당신과의 거리가 {item.dist < 1000 ? `${item.dist} m` : `${(item.dist / 10000).toFixed(2)} km`} </p>
+      </div>
     </>
   );
 }
--- front-workspace/src/container/IndexContainer.jsx
+++ front-workspace/src/container/IndexContainer.jsx
@@ -13,30 +13,43 @@ import CardComponent from "../components/CardComponent";
 import mainStyle from "../resources/css/Main.module.css";
 
 function IndexContainer() {
+  let [myLocation, setMyLocation] = useState(null);
   const [nearestList, setNearestList] = useState(null);
   const [popularList, setPopularList] = useState(null);
 
+  const refreshMyLocation = async () => {
+    setMyLocation(await getLocation());
+  };
+
   const refreshNearestList = async () => {
-    const response = await getNearestList(await getLocation());
+    console.log(myLocation);
+    const response = await getNearestList(myLocation);
     setNearestList(response);
   };
 
   const refreshPopularList = async () => {
-    const response = await getPopularList();
+    console.log(myLocation);
+    const response = await getPopularList(myLocation);
     setPopularList(response);
   };
 
   useEffect(() => {
-    if (!nearestList) {
+    if (!nearestList && myLocation) {
       refreshNearestList();
     }
-  }, [nearestList]); // 가까운 관광지 호출
+  }, [nearestList, myLocation]); // 가까운 관광지 호출
 
   useEffect(() => {
-    if (!popularList) {
+    if (!popularList && myLocation) {
       refreshPopularList();
     }
-  }, [popularList]); // 유명한 관광지 호출
+  }, [popularList, myLocation]); // 유명한 관광지 호출
+
+  useEffect(() => {
+    if (!myLocation) {
+      refreshMyLocation();
+    }
+  }, [myLocation]);
 
   return (
     <>
@@ -46,7 +59,11 @@ function IndexContainer() {
         <div className={mainStyle.popularList}>
           <h3 className={mainStyle.menuTitle}>🏠 가까운 관광지를 확인하세요</h3>
           {nearestList && nearestList.length !== 0 ? (
-            nearestList.map((popular, index) => <CardComponent />)
+            <div className={mainStyle.cardContainer}>
+              {nearestList.map((nearest, index) => (
+                <CardComponent mainStyle={mainStyle} item={nearest} />
+              ))}
+            </div>
           ) : (
             <div className={mainStyle.noContent}>
               🤔 근처 관광지가 없습니다! 관광지 검색을 통해 관광지를 찾아보세요.
@@ -56,7 +73,11 @@ function IndexContainer() {
         <div className={mainStyle.popularList}>
           <h3 className={mainStyle.menuTitle}>🚀 인기 관광지를 확인하세요</h3>
           {popularList && popularList.length !== 0 ? (
-            popularList.map((popular, index) => <CardComponent />)
+            <div className={mainStyle.cardContainer}>
+              {popularList.map((popular, index) => (
+                <CardComponent mainStyle={mainStyle} item={popular} />
+              ))}
+            </div>
           ) : (
             <div className={mainStyle.noContent}>
               🤔 인기 관광지가 없습니다! 하트를 눌러 좋아하는 관광지를 표시하세요.

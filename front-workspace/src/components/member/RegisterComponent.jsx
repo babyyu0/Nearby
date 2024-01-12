ad6de6df85d2122db384f0a632c19291a6efa6a7
유영 <dbdud1999@naver.com>
2024-01-13 02:39:59 +0900

    :art: 행정구역 생성

--- front-workspace/src/components/member/RegisterComponent.jsx
+++ front-workspace/src/components/member/RegisterComponent.jsx
@@ -11,6 +11,7 @@ function LoginComponent({ registerStyle, profile, setProfile, id, setId, isExist
   const fileDOM = useRef();
   const [idAvailable, isIdAvailable] = useState(true);
   const [passwordAvailable, isPasswordAvailable] = useState(true);
+  const [passwordConfirmAvailable, isPasswordConfirmAvailable] = useState(true);
 
   // useEffect(() => {
   //   document.addEventListener('change', (e) => {
@@ -38,6 +39,20 @@ function LoginComponent({ registerStyle, profile, setProfile, id, setId, isExist
     }
   }, [password]);
 
+  useEffect(() => {
+    if (passwordConfirm && password !== passwordConfirm) {
+      isPasswordConfirmAvailable(false);
+    } else {
+      isPasswordConfirmAvailable(true);
+    }
+  }, [passwordConfirm]);
+
+  useEffect(() => {
+    if (name.includes(" ")) {
+      setName(name.replaceAll(" ", ""));
+    }
+  }, [name]);
+
   return (
     <>
       <div className={registerStyle.registerBox}>
@@ -55,9 +70,43 @@ function LoginComponent({ registerStyle, profile, setProfile, id, setId, isExist
           <Tooltip
             id='passwordTooltip'
             place='top-start'
+            opacity='0.7'
           />
           <input type="password" id="password" className={`${registerStyle.infoInput} ${registerStyle.passwordInput}`} value={password} onChange={(e) => { setPassword(e.target.value) }} available={`${passwordAvailable}`} />
           <span className={registerStyle.infoWarning} available={`${passwordAvailable}`}>올바른 비밀번호 형식이 아닙니다.</span>
+        </div>
+        <div className={registerStyle.infoBox}>
+          <label htmlFor="passwordConfirm" className={`${registerStyle.infoLabel} ${registerStyle.passwordConfirmLabel}`}>비밀번호 확인</label>
+          <input type="password" id="passwordConfirm" className={`${registerStyle.infoInput} ${registerStyle.passwordConfirmInput}`} value={passwordConfirm} onChange={(e) => { setPasswordConfirm(e.target.value) }} available={`${passwordConfirmAvailable}`} />
+          <span className={registerStyle.infoWarning} available={`${passwordConfirmAvailable}`}>비밀번호가 일치하지 않습니다.</span>
+        </div>
+        <div className={registerStyle.infoBox}>
+          <label htmlFor="name" className={`${registerStyle.infoLabel} ${registerStyle.nameLabel}`}>이름</label>
+          <input type="text" id="name" className={`${registerStyle.infoInput} ${registerStyle.nameInput}`} value={name} onChange={(e) => { setName(e.target.value) }} /> 
+          </div>
+        <div className={`${registerStyle.infoBox} ${registerStyle.areaBox}`}>
+          <div className={registerStyle.sidoBox}>
+            <label htmlFor="sido" className={`${registerStyle.infoLabel} ${registerStyle.sidoLabel}`}>행정구역 (시 · 도)</label>
+            <select className={`${registerStyle.infoInput} ${registerStyle.sidoInput}`} value={sido} onChange={(e) => { setGugun(0); setSido(e.target.value); }}>
+              {
+                sidoList?
+                  sidoList.map((sido, index) => (
+                    <option value={sido.code} key={index}>{sido.name}</option>
+                    )) : <option value="0" disabled>분류되지 않음</option>
+              }
+            </select>
+          </div>
+          <div className={registerStyle.gugunBox}>
+            <label htmlFor="gugun" className={`${registerStyle.infoLabel} ${registerStyle.gugunLabel}`}>하위 행정구역 (시 · 군 · 구)</label>
+            <select className={`${registerStyle.infoInput} ${registerStyle.gugunInput}`} value={gugun} onChange={(e) => { setGugun(e.target.value); }}>
+            {
+                gugunList && gugunList[sido] && sido?
+                  gugunList[sido].map((gugun, index) => (
+                    <option value={gugun.code} key={index}>{gugun.name}</option>
+                    )) : <option value="0" disabled>분류되지 않음</option>
+              }
+            </select>
+          </div>
         </div>
       {/* 
       <div className={registerStyle.registerContainer}>

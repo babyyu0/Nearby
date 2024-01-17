d86732116d58722798822abd1c89cf312c63763b
유영 <dbdud1999@naver.com>
2024-01-17 21:01:07 +0900

    :lipstick: 배너 레이아웃 구성

--- front-workspace/src/components/member/BannerComponent.jsx
+++ front-workspace/src/components/member/BannerComponent.jsx
@@ -1,8 +1,11 @@
-function BannerComponent({ registerStyle, text }) {
+function BannerComponent({ registerStyle, imgUrl, text }) {
   return (
-    <>
-      <div className={registerStyle.bannerBox}>{text}</div>
-    </>
+    <div className={registerStyle.bannerBox}>
+      <div className={registerStyle.bannerImg}>
+        <img src={imgUrl} alt="carousal 이미지" />
+      </div>
+      <div className={registerStyle.bannerDesc}>{text}</div>
+    </div>
   );
 }
 
--- front-workspace/src/components/member/RegisterDescComponent.jsx
+++ front-workspace/src/components/member/RegisterDescComponent.jsx
@@ -19,9 +19,9 @@ function RegisterDescComponent({ registerStyle }) {
     <>
       <div className={registerStyle.registerDescBox}>
         <Slider {...sliderSetting}>
-          <BannerComponent registerStyle={registerStyle} text={`text A`} />
-          <BannerComponent registerStyle={registerStyle} text={`text B`} />
-          <BannerComponent registerStyle={registerStyle} text={`text C`} />
+          <BannerComponent registerStyle={registerStyle} imgUrl={`${process.env.PUBLIC_URL}/image/banner/desc01.png`} text={` 오늘의 인기 관광지를 확인해 보세요 🚀`} />
+          <BannerComponent registerStyle={registerStyle} imgUrl={`${process.env.PUBLIC_URL}/image/banner/desc02.png`} text={`거리를 비교하며 가장 가까운 관광지를 찾아갈 수 있어요 🗺️`} />
+          <BannerComponent registerStyle={registerStyle} imgUrl={`${process.env.PUBLIC_URL}/image/banner/desc03.png`} text={`함께 여행할 친구들도 모을 수 있답니다 😎`} />
         </Slider>
       </div>
     </>

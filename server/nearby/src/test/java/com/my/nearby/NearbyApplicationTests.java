f1e3c94fea4d8d9f5a1c4d6ecba6dc7e4a289646
유영 <dbdud1999@naver.com>
2024-04-11 01:43:06 +0900

    #10 서비스 새로 만들기

---
+++ server/nearby/src/test/java/com/my/nearby/NearbyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.my.nearby;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NearbyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

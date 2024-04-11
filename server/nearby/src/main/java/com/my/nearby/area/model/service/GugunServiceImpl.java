ae8a221ea464512639981daae9fbb633ca303b8f
유영 <dbdud1999@naver.com>
2024-04-12 03:03:57 +0900

    #10 :sparkles: 행정구역 Refresh 기능 추가

--- server/nearby/src/main/java/com/my/nearby/area/model/service/GugunServiceImpl.java
+++ server/nearby/src/main/java/com/my/nearby/area/model/service/GugunServiceImpl.java
@@ -1,6 +1,14 @@
-package com.my.nearby.area;
+package com.my.nearby.area.model.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.my.nearby.BusinessException;
+import com.my.nearby.area.model.dao.GugunDao;
+import com.my.nearby.area.model.dto.AreaCodeDto;
+import com.my.nearby.area.model.dto.GugunResponseDto;
+import com.my.nearby.area.model.vo.GugunVo;
+import com.my.nearby.area.util.KorServiceApiUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
@@ -13,11 +21,12 @@ import java.util.List;
 @RequiredArgsConstructor
 public class GugunServiceImpl implements GugunService {
     private final GugunDao gugunDao;
+
     @Override
     @Transactional(readOnly = true)
     public GugunResponseDto findGugun(int gugunCode) {
         GugunVo gugun = gugunDao.select(gugunCode);
-        if(gugun == null) {
+        if (gugun == null) {
             throw new BusinessException(HttpStatus.BAD_REQUEST, "일치하는 하위 행정구역이 존재하지 않습니다.");
         }
 
@@ -25,30 +34,55 @@ public class GugunServiceImpl implements GugunService {
     }
 
     @Override
+    @Transactional(readOnly = true)
     public List<GugunResponseDto> findAllGugun() {
         List<GugunVo> gugunList = gugunDao.selectAll();
         List<GugunResponseDto> gugunResponseDtoList = new ArrayList<>();
-        if(gugunList == null || gugunList.isEmpty()) {
+        if (gugunList == null || gugunList.isEmpty()) {
             throw new BusinessException(HttpStatus.BAD_REQUEST, "일치하는 하위 행정구역이 존재하지 않습니다.");
         }
 
-        for(GugunVo gugun : gugunList) {
+        for (GugunVo gugun : gugunList) {
             gugunResponseDtoList.add(GugunResponseDto.from(gugun));
         }
         return gugunResponseDtoList;
     }
 
     @Override
+    @Transactional(readOnly = true)
     public List<GugunResponseDto> findAllGugunBySidoCode(int sidoCode) {
         List<GugunVo> gugunList = gugunDao.selectAllBySidoCode(sidoCode);
         List<GugunResponseDto> gugunResponseDtoList = new ArrayList<>();
-        if(gugunList == null || gugunList.isEmpty()) {
+        if (gugunList == null || gugunList.isEmpty()) {
             throw new BusinessException(HttpStatus.BAD_REQUEST, "일치하는 하위 행정구역이 존재하지 않습니다.");
         }
-        
-        for(GugunVo gugun : gugunList) {
+
+        for (GugunVo gugun : gugunList) {
             gugunResponseDtoList.add(GugunResponseDto.from(gugun));
         }
         return gugunResponseDtoList;
+    }
+
+    @Override
+    @Transactional(rollbackFor = RuntimeException.class)
+    public void refreshGugun(int sidoCode) {
+        JsonNode jsonNode = KorServiceApiUtil.getAreaCode(Integer.toString(sidoCode));
+        ObjectMapper objectMapper = new ObjectMapper();
+        List<AreaCodeDto> gugunDtoList = new ArrayList<>();
+        gugunDtoList.add(new AreaCodeDto(0, 0, "분류되지 않음"));
+
+        int i = 0;
+        try {
+            while (i < jsonNode.size()) {
+                gugunDtoList.add(objectMapper.treeToValue(jsonNode.get(i++), AreaCodeDto.class));
+            }
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e.getMessage());
+        }
+
+        for (AreaCodeDto gugunDto : gugunDtoList) {
+            GugunVo gugun = new GugunVo(gugunDto.code(), sidoCode, gugunDto.name());
+            gugunDao.insert(gugun);
+        }
     }
 }

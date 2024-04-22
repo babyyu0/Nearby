87232d089205a8c2ec95024b574bdf8d6c317f94
유영 <dbdud1999@naver.com>
2024-04-22 15:50:27 +0900

    #10 :sparkles: Refresh API 추가

--- server/nearby/src/main/java/com/my/nearby/area/controller/AreaController.java
+++ server/nearby/src/main/java/com/my/nearby/area/controller/AreaController.java
@@ -1,24 +1,33 @@
 package com.my.nearby.area.controller;
 
+import com.my.nearby.area.model.dto.AreaResponseDto;
 import com.my.nearby.area.model.service.AreaService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/area")
 @RequiredArgsConstructor
 public class AreaController {
     private final AreaService areaService;
+
     @GetMapping
     public ResponseEntity<?> getAreaList() {
         return ResponseEntity.ok(areaService.findAllArea());
     }
-    @GetMapping("/{areaCode}/{sigunguCode}")
-    public ResponseEntity<?> getArea(@PathVariable int areaCode, @PathVariable int sigunguCode) {
-        return ResponseEntity.ok(areaService.findArea(areaCode, sigunguCode));
+
+    @GetMapping("refresh")
+    public ResponseEntity<?> refreshAllArea() {
+        List<AreaResponseDto> areaDtoList = areaService.refreshArea();
+        for (AreaResponseDto areaDto : areaDtoList) {
+            areaService.refreshSigungu(areaDto.areaCode());
+
+        }
+        return ResponseEntity.ok("");
     }
 }
